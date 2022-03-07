6b4e03d0fcdd1a43d168504cadae4c38eac6842e
MatiasGonzalez1 <83165602+MatiasGonzalez1@users.noreply.github.com>
2022-03-07 12:44:28 -0300

    carousel funcional

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,69 @@
 let overlay = document.querySelector('.overlay');
 let carousel = document.querySelector('.carousel');
+let slides = document.querySelectorAll('.carousel .slide');
+let images = document.querySelectorAll('.carousel .slide img')
+let rightArrow = document.querySelector('.right-arrow');
+let leftArrow = document.querySelector('.left-arrow');
+let counter = 0;
+
+function loadSlides() {
+    carousel.style.height = '90vh';
+    let heightCarousel = carousel.offsetHeight;
+    for (i = 0; i < images.length; i++) {
+        if(images[i].offsetHeight < heightCarousel) {
+            heightCarousel = images[i].offsetHeight;
+        }
+    }
+
+    carousel.style.height = heightCarousel + 'px';
+
+    for(i=0; i < slides.length; i++){
+    slides[i].style.left =  carousel.offsetWidth * -i + 'px';
+}}
+
 
 function openModal(){
     overlay.style.display = 'block';
     carousel.style.display = 'block';
+    loadSlides();
 }
 
 function closeModal(){
     overlay.style.display = 'none';
     carousel.style.display = 'none';
+}
+
+function nextSlide(){
+    for (i=0; i < slides.length; i++){
+        slides[i].style.left =  slides[i].offsetLeft + carousel.offsetWidth + 'px';
+    }
+    updateArrowState(1);
+}
+function prevSlide(){
+    
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.left =  slides[i].offsetLeft - carousel.offsetWidth + 'px';
+    }
+    updateArrowState(-1)
+
+}
+
+function updateArrowState(e){
+    counter += e;
+    if(counter !== 0){
+        leftArrow.style.display = 'flex';
+    } else {
+        leftArrow.style.display = 'none';
+    }
+    if (counter === slides.length - 1){
+        rightArrow.style.display = 'none';
+    } else {
+        rightArrow.style.display = 'flex'
+    }
+    rightArrow.style.pointerEvents = 'none';
+    leftArrow.style.pointerEvents = 'none';
+    setTimeout(() => {
+        rightArrow.style.pointerEvents = 'auto';
+    leftArrow.style.pointerEvents = 'auto';
+    }, 900)
 }
